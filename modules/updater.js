c7acc54fa09e38ba270fd7a2720f2b7efd208195
jely2002 <jw.glebbeek@gmail.com>
2020-09-01 11:40:10 +0200

    Add error handling to updater.js, fixing #10.

--- modules/updater.js
+++ modules/updater.js
@@ -28,66 +28,99 @@ update()
 //Probes youtube-dl for new updates, and downloads them when needed.
 function update() {
     binaryUpdating(true)
-    request.get(url, { followRedirect: false }, function (err, res) {
+    request.get(url, { followRedirect: false }, async function (err, res) {
         if (err) {
             console.log(err)
             if(err.toString().includes('ENOTFOUND')) {
                 $('#connection').toast('show')
                 $('#connection').css('visibility','visible')
                 $('#url').prop("disabled", true).attr("placeholder", "Please connect to the internet and restart this app")
+                binaryUpdating(false)
             }
             return
         }
         if (res.statusCode !== 302) {
+            binaryUpdating(false)
             return console.log('Did not get redirect for the latest version link. Status: ' + res.statusCode)
         }
         const newUrl = res.headers.location
         const newVersion = /yt-dl\.org\/downloads\/(\d{4}\.\d\d\.\d\d(\.\d)?)\/youtube-dl/.exec(newUrl)[1]
         console.log("Latest release: " + newVersion)
-        if(newVersion === getCurrentVersion()) {
+        let currentVersion = await getCurrentVersion()
+        if(newVersion === currentVersion) {
             binaryUpdating(false)
             console.log("Binaries were already up-to-date!")
+        } else if(currentVersion == null) {
+            console.log("Binaries may be corrupted, reinstalling...")
+            downloadUpdate(newUrl, newVersion)
         } else {
             console.log("New version found! Updating...")
-            const downloadFile = request.get(newUrl)
-            downloadFile.on('response', function response(res) {
-                if (res.statusCode !== 200) {
-                    console.log('Response Error: ' + res.statusCode)
-                    return
-                }
-                downloadFile.pipe(fs.createWriteStream(filePath, {mode: 493}))
-            })
-            downloadFile.on('error', function error(err) {
-                console.log(err)
-            })
-            downloadFile.on('end', function end() {
-                binaryUpdating(false)
-                console.log("New youtube-dl version downloaded: " + newVersion)
-                console.log("Writing version data...")
-                fs.writeFileSync(
-                    defaultPath,
-                    JSON.stringify({
-                        version: newVersion,
-                        path: filePath
-                    }),
-                    'utf8'
-                )
-                if(process.platform === "darwin" || process.platform === "linux") {
-                    console.log("Adding chmod permissions")
-                    fs.chmod(filePath, 0o755, function(err){
-                        if(err) console.log(err)
-                        console.log("chmod 0755 added")
-                    })
-                }
-            })
+            downloadUpdate(newUrl, newVersion)
         }
     })
 }
 
 //Shows the currently downloaded version of youtube-dl
-function getCurrentVersion() {
-    let details = JSON.parse(fs.readFileSync(defaultPath, 'utf-8'))
-    console.log("Current version: " + details.version)
-    return details.version;
+async function getCurrentVersion() {
+    const call = new Promise((resolve, reject) => {
+        fs.readFile(defaultPath, (err, data) => {
+            if (err) {
+                console.error(err)
+                if (err.code === "ENOENT") {
+                    resolve(null)
+                    return
+                }
+            }
+            let detailsData
+            try {
+                detailsData = JSON.parse(data)
+            } catch (error) {
+                console.error(error)
+                resolve(null)
+                return
+            }
+            resolve(detailsData)
+        });
+    })
+    let details = await call
+    if(details == null) {
+        return null
+    } else {
+        console.log("Current version: " + details.version)
+        return details.version
+    }
+}
 
+function downloadUpdate(newUrl, newVersion) {
+    const downloadFile = request.get(newUrl)
+    downloadFile.on('response', function response(res) {
+        if (res.statusCode !== 200) {
+            console.log('Response Error: ' + res.statusCode)
+            return
+        }
+        downloadFile.pipe(fs.createWriteStream(filePath, {mode: 493}))
+    })
+    downloadFile.on('error', function error(err) {
+        console.log(err)
+    })
+    downloadFile.on('end', function end() {
+        binaryUpdating(false)
+        console.log("New youtube-dl version downloaded: " + newVersion)
+        console.log("Writing version data...")
+        fs.writeFileSync(
+            defaultPath,
+            JSON.stringify({
+                version: newVersion,
+                path: filePath
+            }),
+            'utf8'
+        )
+        if (process.platform === "darwin" || process.platform === "linux") {
+            console.log("Adding chmod permissions")
+            fs.chmod(filePath, 0o755, function (err) {
+                if (err) console.log(err)
+                console.log("chmod 0755 added")
+            })
+        }
+    })
 }
