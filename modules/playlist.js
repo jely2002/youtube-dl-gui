90c1010898977279d42c792065315fa0ccac7187
Jelle Glebbeek <jw.glebbeek@gmail.com>
2020-11-16 23:35:28 +0100

    Fix downloader not using the right format_ids when downloading playlists

--- modules/playlist.js
+++ modules/playlist.js
@@ -262,10 +262,10 @@ function downloadPlaylist(audioQuality) {
     setProgressBarText(false, "Video %1 of %2 downloaded", videosDownloaded, amountToDownload)
     if(process.platform === "win32") ipcRenderer.invoke('setOverlayIcon', {mode: "downloading"})
 
-    function downloadVideo(item, format_id, queue, cb) {
+    function downloadVideo(item, format_id, queue, number, cb) {
         if(item.removed === "yes") {
             ++videosDownloaded
-            queue++
+            increaseQueue(number)
             let percentage = ((videosDownloaded / amountToDownload) * 100) + "%"
             setProgressBarProgress(false, videosDownloaded, amountToDownload)
             setProgressBarText(false, "Video %1 of %2 downloaded", videosDownloaded, amountToDownload)
@@ -309,7 +309,7 @@ function downloadPlaylist(audioQuality) {
             options.push('--cookies')
             options.push(cookiePath)
         }
-        queue++
+        increaseQueue(number)
         callYTDL(item.webpage_url, options, {}, false, function(err, output) {
             if (err) showError(err)
             ++videosDownloaded
@@ -328,25 +328,25 @@ function downloadPlaylist(audioQuality) {
 
     let videometadata1 = halfSlice1.reduce((promiseChain, item) => {
         return promiseChain.then(() => new Promise((resolve) => {
-            downloadVideo(item, formatSlice1, procOneQueue, resolve)
+            downloadVideo(item, formatSlice1, procOneQueue, 1, resolve)
         }))
     }, Promise.resolve())
 
     let videometadata2 = halfSlice2.reduce((promiseChain, item) => {
         return promiseChain.then(() => new Promise((resolve) => {
-            downloadVideo(item, formatSlice2, procTwoQueue, resolve)
+            downloadVideo(item, formatSlice2, procTwoQueue, 2, resolve)
         }))
     }, Promise.resolve())
 
     let videometadata3 = quarterSlice1.reduce((promiseChain, item) => {
         return promiseChain.then(() => new Promise((resolve) => {
-            downloadVideo(item, formatSlice3, procThreeQueue, resolve)
+            downloadVideo(item, formatSlice3, procThreeQueue, 3, resolve)
         }))
     }, Promise.resolve())
 
     let videometadata4 = quarterSlice2.reduce((promiseChain, item) => {
         return promiseChain.then(() => new Promise((resolve) => {
-            downloadVideo(item, formatSlice4, procFourQueue, resolve)
+            downloadVideo(item, formatSlice4, procFourQueue, 4, resolve)
         }))
     }, Promise.resolve())
 
@@ -369,4 +369,16 @@ function downloadPlaylist(audioQuality) {
         fourthSideResolved = true
         done()
     })
+
+    function increaseQueue(number) {
+        if(number === 1) {
+            procOneQueue++
+        } else if(number === 2) {
+            procTwoQueue++
+        } else if(number === 3) {
+            procThreeQueue++
+        } else if(number === 4) {
+            procFourQueue++
+        }
+    }
 }
