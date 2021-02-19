e67c7e9b6514e375f5904d3f8cce08a89a5176f2
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-02-19 11:40:49 +0100

    Add errorReport channel to preload.js

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld(
     "main", {
         invoke: async (channel, data) => {
-            let validChannels = ["platform", "messageBox", "titlebarClick", "openInputMenu", "settingsAction", "videoAction", "cookieFile", "downloadFolder"];
+            let validChannels = ["platform", "messageBox","errorReport", "titlebarClick", "openInputMenu", "settingsAction", "videoAction", "cookieFile", "downloadFolder"];
             if (validChannels.includes(channel)) {
                 return await ipcRenderer.invoke(channel, data);
             }
