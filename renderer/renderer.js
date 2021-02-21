18094ddb45c4c9dccab6548c4d3c388e53e2056b
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-02-21 02:19:20 +0100

    Fix the windowbar title not being centered in MacOS

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -12,7 +12,7 @@ async function init() {
     if(platform === "darwin") {
         new windowbar({'style':'mac', 'dblClickable':false, 'fixed':true, 'title':document.title,'dark':true})
             .appendTo(document.body)
-        $('.windowbar-title').css("left", "50%").css("top", "calc(32px /2)");
+        $('.windowbar-title').css("left", "50%").css("top", "14px");
         $('.windowbar-controls').css("display", "none");
     } else {
         new windowbar({'style':'win', 'dblClickable':false, 'fixed':true, 'title':document.title,'dark':true})
