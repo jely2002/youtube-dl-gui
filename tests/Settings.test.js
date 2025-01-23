9fe6c81cbce8d8832ed392a0ce18ed8be4b54ceb
mp3butcher <julienvalentin51@gmail.com>
2025-01-24 00:06:35 +0100

    fix defaults settings in tests

--- tests/Settings.test.js
+++ tests/Settings.test.js
@@ -16,7 +16,7 @@ jest.mock('electron', () => ({
 const Settings = require('../modules/persistence/Settings');
 const env = {version: '2.0.0-test1', app: {getPath: jest.fn().mockReturnValue('test/path')}};
 var defaultSettingsInstance = new Settings({settings: 'tests/test-settings.json'}, env, 'none', 'none', 'test/path', '', '', true, false, true, 'spoof', false, false, true, '%(title).200s-(%(height)sp%(fps).0d).%(ext)s', '%(title).200s-(%(height)sp%(fps).0d).%(ext)s', 'click', '49', 8, 6, 2, true, 'video', true, 'C:\\Users\\user\\cookies.txt', false, '', '', 'https://sponsor.ajay.app', true, false, false, false, false, false, true, 'dark');
-defaultSettingsInstance.setupMitmproxyConfig(15930, '--anticache --anticomp', ['if-range']);
+defaultSettingsInstance.setupMitmproxyConfig(15930, "--anticache --anticomp --mode socks5", ["if-range", "if-none-match", "if-modified-since", "if-match", "if-unmodified-since", "sec-ch-ua"]);
 const defaultSettings = {
   outputFormat: 'none',
   audioOutputFormat: 'none',
@@ -52,7 +52,7 @@ const defaultSettings = {
   avoidFailingToSaveDuplicateFileName: false,
   allowUnsafeFileExtensions: false,
   mitmPort: 15930,
-  mitmExtraArgs: '--anticache --anticomp',
+  mitmExtraArgs: "--anticache --anticomp --mode socks5",
   headerFilter: ["if-range", "if-none-match", "if-modified-since", "if-match", "if-unmodified-since", "sec-ch-ua"],
   calculateTotalSize: true,
   theme: 'dark',
