901bc2f8861cd2189781a7affe9defcd6d591499
Jelle Glebbeek <jw.glebbeek@gmail.com>
2025-12-13 15:09:05 +0100

    i18n: enable French language

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,11 +2,12 @@ import { createI18n, I18n } from 'vue-i18n';
 import en from './locales/en.json';
 import nl from './locales/nl.json';
 import it from './locales/it.json';
+import fr from './locales/fr.json';
 import ptBR from './locales/pt-BR.json';
 import { detectBrowserLanguageCodes } from './helpers/subtitles/languages.ts';
 
 export const availableLocales: Record<string, boolean> = {
-  'en': true, 'nl': true, 'it': true, 'pt-BR': true,
+  'en': true, 'nl': true, 'it': true, 'fr': true, 'pt-BR': true,
 } as const;
 
 type MessageSchema = typeof en;
@@ -29,6 +30,7 @@ export const i18n: I18n = createI18n<[MessageSchema], Locale>({
     en,
     nl,
     it,
+    fr,
     'pt-BR': ptBR,
   },
 });
