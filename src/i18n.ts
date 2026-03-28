d17d2de3ab126f2282a6b71848514fc3e6252532
Jelle Glebbeek <jelle@jelleglebbeek.com>
2026-03-28 21:44:00 +0100

    feat: enable turkish locale in the app and installer

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,12 +7,13 @@ import fr from './locales/fr.json';
 import de from './locales/de.json';
 import nb from './locales/nb.json';
 import ru from './locales/ru.json';
+import tr from './locales/tr.json';
 import ptBR from './locales/pt-BR.json';
 import zhTW from './locales/zh-TW.json';
 import { detectBrowserLanguageCodes } from './helpers/subtitles/languages.ts';
 
 export const availableLocales: Record<string, boolean> = {
-  'en': true, 'es': true, 'nl': true, 'it': true, 'fr': true, 'de': true, 'nb': true, 'ru': true, 'pt-BR': true, 'zh-TW': true,
+  'en': true, 'es': true, 'nl': true, 'it': true, 'fr': true, 'de': true, 'nb': true, 'ru': true, 'tr': true, 'pt-BR': true, 'zh-TW': true,
 } as const;
 
 type MessageSchema = typeof en;
@@ -40,6 +41,7 @@ export const i18n: I18n = createI18n<[MessageSchema], Locale>({
     de,
     nb,
     ru,
+    tr,
     'pt-BR': ptBR,
     'zh-TW': zhTW,
   },
